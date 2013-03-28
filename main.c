888bf329a1f3ec5710cf3cf2efb2b5bfe1a9327f
Mateus Galvão <mateus@uololou.com.br>
2013-03-28 16:21:42 -0300

    Alterando pipe

--- main.c
+++ main.c
@@ -1,52 +1,36 @@
-#include <sys/wait.h>
-       #include <stdio.h>
-       #include <stdlib.h>
-       #include <unistd.h>
-       #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-       int
-       main(int argc, char *argv[])
-       {
-           int pipefd[2];
-           pid_t cpid;
-           char buf;
+int pp[2];
+int pid;
 
-           *buf = "a";
+/************************** ATENÇÃO: **************************************
+ * Este programa é apenas um exemplo de como se usar dup2 para redirecionar
+ * a entrada e a saída padrão de um programa. Por brevidade, ele foi
+ * escrito sem maiores preocupações com o teste dos valores de retorno das
+ * chamadas de sistema usadas, mas qualquer código completo desenvolvido
+ * deve obrigatoriamente fazer o tratamento adequado de erro em todos os
+ * casos em que isso for necessário.
+ **************************************************************************/
 
-           if (argc != 2) {
-            fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-            exit(EXIT_FAILURE);
-           }
+int main(int argc , char* argv[])
+{
+    int pid;
 
-           if (pipe(pipefd) == -1) {
-               perror("pipe");
-               exit(EXIT_FAILURE);
-           }
+    if ( pipe(pp) < 0 ) exit(1);
 
-           cpid = fork();
-           if (cpid == -1) {
-               perror("fork");
-               exit(EXIT_FAILURE);
-           }
-
-           if (cpid == 0) {    /* Child reads from pipe */
-               close(pipefd[1]);          /* Close unused write end */
-
-               while (read(pipefd[0], &buf, 1) > 0)
-                   write(STDOUT_FILENO, &buf, 1);
-
-               write(STDOUT_FILENO, "\n", 1);
-               close(pipefd[0]);
-               _exit(EXIT_SUCCESS);
-
-
-           } else {            /* Parent writes argv[1] to pipe */
-               close(pipefd[0]);          /* Close unused read end */
-               write(pipefd[1], argv[1], strlen(argv[1]));
-               close(pipefd[1]);          /* Reader will see EOF */
-               wait(NULL);                /* Wait for child */
-               exit(EXIT_SUCCESS);
-           }
-       }
-
-
+    if ( ( pid = fork() ) < 0 ) exit(1);
+    if (pid == 0) {     /* filho */
+       close(pp[0]);    /* fecha saída do pipe (só vai escrever) */
+       dup2(pp[1], 1);  /* associa entrada do pipe com saída padrão */
+       close(pp[1]);    /* fecha pp[1], pois se tornou redundante */
+       execlp(argv[2],argv[2],NULL);
+    } else {            /* pai */
+       close( pp[1] );  /* fecha entrada do pipe (só vai ler) */
+       dup2(pp[0], 0);  /* associa saída do pipe com entrada padrão */
+       close(pp[0]);    /* fecha pp[0], pois se tornou redundante */
+       execlp(argv[1],argv[1],NULL);
+    }
+    return 0;
+}
