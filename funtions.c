52114e64a8c4dc03efa7adbaf63c4619b0cd9112
Mateus Galvão <mateus@uololou.com.br>
2013-03-31 19:36:58 -0300

    Finalizando parse dos argumentos

--- funtions.c
+++ funtions.c
@@ -15,18 +15,35 @@ void executaProcesso(int num_args ,char *argv[]){
 }
 
 char** parseCommands(char* comando){
+//void parseCommands(char* comando){
 
     int i = 0;
+    int j,h;
     char* delims = " ";
     char* result = NULL;
     char* words[512];
+    char* f_words[512];
+
     result = strtok(comando,delims);
     while( result != NULL ) {
         words[i] = result;
         result = strtok( NULL , delims );
         i++;
     }
+
+    j = i;
     words[i] = NULL;
-    return words;
+    h=0;
+    for(i=0;i<j;i++){
+        result = strtok(words[i],"\t");
+        while( result != NULL ){
+            f_words[h] = result;
+            result = strtok(NULL,"\t");
+            h++;
+        }
+    }
+
+    f_words[h] = NULL;
+    return f_words;
 
 }
